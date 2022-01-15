9f3c2dbb2fccb7771b3543f5d4040bc520f4bce2
Sebastian Fuhrmann <sebastian.fuhrmann@rz-fuhrmann.de>
2022-01-15 17:58:13 +0100

    fix einrückung

--- api/getLocation.php
+++ api/getLocation.php
@@ -52,8 +52,8 @@
 
     http_response_code($res["code"]);
     header('Content-Type: application/json');
-	header('Pragma: no-cache');
-	header('Expires: Fri, 01 Jan 1990 00:00:00 GMT');
-	header('Cache-Control: no-cache, no-store, must-revalidate');
+    header('Pragma: no-cache');
+    header('Expires: Fri, 01 Jan 1990 00:00:00 GMT');
+    header('Cache-Control: no-cache, no-store, must-revalidate');
     echo json_encode($res, JSON_PRETTY_PRINT);
 ?>
