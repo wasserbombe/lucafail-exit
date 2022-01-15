f463c6e843247aa2c5875994a47678e406229e79
Sebastian Fuhrmann <sebastian.fuhrmann@rz-fuhrmann.de>
2022-01-15 17:25:27 +0100

    add table

--- api/getLocation.php
+++ api/getLocation.php
@@ -3,17 +3,40 @@
         "data" => array(),
         "code" => 500
     ); 
+    function performGETRequest($url){
+        $cache_fn = __DIR__."/cache/".md5($url).".json";
+
+        // disable requests to Luca API; use local sample data instead
+        if (preg_match("~/scanners/~", $url)){
+            $cache_fn = __DIR__."/cache/a7bf3cbba401864c40feabe9d6685558.json";
+        } else {
+            $cache_fn = __DIR__."/cache/6ced03b5f62f3b2b9b229da43e11de29.json";
+        }
+
+        if (file_exists($cache_fn)){
+            $result = file_get_contents($cache_fn);
+        } else {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            file_put_contents($cache_fn, $result);
+        }
+        
+        return json_decode($result, true);
+    }
     if (isset($_REQUEST["id"])){
         
         // get scanner
-        $scanner = file_get_contents("https://app.luca-app.de/api/v3/scanners/".preg_replace("~[^a-z0-9-]~i", "", $_REQUEST["id"]));
+        $scanner = performGETRequest("https://app.luca-app.de/api/v3/scanners/".preg_replace("~[^a-z0-9-]~i", "", $_REQUEST["id"]));
 
-        $scanner = json_decode($scanner, true);
         // get location
-        $location = file_get_contents("https://app.luca-app.de/api/v3/locations/".$scanner["locationId"]);
+        $location = performGETRequest("https://app.luca-app.de/api/v3/locations/".$scanner["locationId"]);
 
-        $location = json_decode($location, true);
         if ($location && !empty($location["name"])){
+            // don't return Luca's plain location JSON as it holds sensitive data...
             $locRes = array(
                 "name" => $location["name"],
                 "groupName" => $location["groupName"],
@@ -21,10 +44,7 @@
             ); 
 
             $res["data"] = $locRes; 
-            $res["dbg"] = array(
-                "scanner" => $scanner,
-                "location" => $location
-            );
+
             $res["code"] = 200;
         }
 
--- js/main.js
+++ js/main.js
@@ -1,14 +1,61 @@
 (function(){
-    function onScanSuccess(decodedText, decodedResult) {
-        
-        console.log("Match: ", decodedText, decodedResult);
+    var locationNames = {}; 
+    var qrs = [];
+    function getLocationName(id){
+        if (typeof locationNames[id] == 'undefined'){
+            $.ajax({
+                "url": "/api/getLocation.php?id=" + id,
+                "success": function(data){
+                    locationNames[id] = data.data;
+                }
+            });
+        }
 
+        return locationNames[id];
+    }
+    function updateTable(){
+        
+        $("#table tbody").html(""); 
+        for (var q = 0; q < qrs.length; q++){
+            var $tr = $("<tr>");
+            $tr.append($("<td>").text(qrs[q].groupName));
+            $tr.append($("<td>").text(qrs[q].locationName));
+            $tr.append($("<td>").text(qrs[q].data?atob(qrs[q].data):""));
+
+            $("#table tbody").append($tr);
+        }
+       
+    }
+    function addQRIfNotExists(qr){
+        var found = false; 
+        for (var q = 0; q < qrs.length; q++){
+            if (qrs[q].name == qr.name && qrs[q].data == qr.data){
+                found = true; 
+                break; 
+            }
+        }
+
+        if (!found){
+            qrs.push(qr);
+            updateTable();
+        }
+    }
+    function onScanSuccess(decodedText, decodedResult) {
         try {
             var lucaURL = new URL (decodedText); 
             var lucaID = lucaURL.pathname.replace(/^\/webapp\//, '');
-            $.ajax({
-                "url": "/_temp/scandata.php?data=" + btoa(lucaURL + ' / ' + lucaID)
-            });
+            var lname = getLocationName(lucaID);
+
+            if (lname && lname.name){
+                lname.data = ""; 
+
+                var dataMatch = lucaURL.hash.match(/#([^\/]+)/i);
+                if (dataMatch){
+                    lname.data = dataMatch[1]; 
+                }
+                addQRIfNotExists({name: lname.name, data: lname.data, locationName: lname.locationName, groupName: lname.groupName});
+            } 
+            
         } catch {};
     }
     
